33209a84d6c92ceff6cd20ac852171edd1bed703
Dutch <carnplaynl@gmail.com>
2020-02-05 12:13:35 +0100

    Added train

---
+++ Assets/Scripts/MetroGen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetroGen : MonoBehaviour {
+
+    [SerializeField] float pieceDistance = 3;
+    [SerializeField] GameObject[] piecePrefabs;
+    public float pieceCount = 2;
+
+    private void Start() {
+        for (int i = 0; i < pieceCount; i++) {
+            GameObject.Instantiate(piecePrefabs[Random.Range(0,piecePrefabs.Length)],transform.position + transform.forward * i * pieceDistance,Quaternion.identity,transform);
+        }
+
+        //place front and back
+    }
+}
