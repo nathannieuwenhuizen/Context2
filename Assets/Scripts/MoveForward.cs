86924dbc5f7c3afb9a18b59598407283f01be93a
DutchDinosaur <carnplaynl@gmail.com>
2020-02-07 00:25:33 +0100

    added metro tube

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -6,10 +6,16 @@ public class MoveForward : MonoBehaviour
 {
     public bool moving;
     public float speed = 1;
+    public float maxDist = 200;
+
+    Vector3 startpos;
+
+    private void Start() {
+        startpos = transform.position;
+    }
 
     void Update() {
-        if (moving) {
-            transform.position += transform.forward * speed * Time.deltaTime;
-        }
+        if (moving) transform.position += transform.forward * speed * Time.deltaTime;
+        if (Vector3.Distance(startpos,transform.position) > maxDist) transform.position = startpos;
     }
 }
